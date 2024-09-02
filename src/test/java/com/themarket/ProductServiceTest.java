9819b1b56aae9bf4c6bf8153486a5b6b797ed201
eva <lupmika@naver.com>
2024-09-02 22:37:07 +0900

    테스트코드 추가

--- src/test/java/com/themarket/ProductServiceTest.java
+++ src/test/java/com/themarket/ProductServiceTest.java
@@ -2,6 +2,7 @@ package com.themarket;
 
 import com.themarket.dto.ProductStockDecreaseRequestDTO;
 import com.themarket.entity.Product;
+import com.themarket.exception.ResourceNotFoundException;
 import com.themarket.exception.ValidationException;
 import com.themarket.repository.ProductRepository;
 import com.themarket.service.ProductService;
@@ -42,6 +43,52 @@ public class ProductServiceTest {
         Product updatedProduct = productRepository.findById(prdNo)
                 .orElseThrow(() -> new RuntimeException("해당 상품번호와 일치하는 정보가 없습니다: " + prdNo));
         assertEquals(400, updatedProduct.getPrdStock(), "재고가 예상한 만큼 차감되지 않았습니다.");
+    }
+
+    @DisplayName("상품재고차감 실패: 상품번호 null")
+    @Test
+    @Transactional
+    public void decreaseStockFail_prdNoNull() {
+        int decreaseQty = 100;
+
+        ProductStockDecreaseRequestDTO requestDTO = ProductStockDecreaseRequestDTO.builder()
+                .decreaseQty(decreaseQty)
+                .build();
+        ValidationException exception = assertThrows(ValidationException.class, () -> productService.decreaseStock(requestDTO));
+        assertEquals("상품번호 혹은 차감개수가 잘못되었습니다.", exception.getMessage());
+
+    }
+
+    @DisplayName("상품재고차감 실패: 음수 개수")
+    @Test
+    @Transactional
+    public void decreaseStockFail_minusQuantity() {
+        String prdNo = "PD240831001";
+        int decreaseQty = -100;
+
+        ProductStockDecreaseRequestDTO requestDTO = ProductStockDecreaseRequestDTO.builder()
+                .prdNo(prdNo)
+                .decreaseQty(decreaseQty)
+                .build();
+        ValidationException exception = assertThrows(ValidationException.class, () -> productService.decreaseStock(requestDTO));
+        assertEquals("상품번호 혹은 차감개수가 잘못되었습니다.", exception.getMessage());
+
+    }
+
+    @DisplayName("상품재고차감 실패: 존재하지 않는 상품번호")
+    @Test
+    @Transactional
+    public void decreaseStockFail_invalidPrdNo() {
+        String prdNo = "PD2408310012345";
+        int decreaseQty = 100;
+
+        ProductStockDecreaseRequestDTO requestDTO = ProductStockDecreaseRequestDTO.builder()
+                .prdNo(prdNo)
+                .decreaseQty(decreaseQty)
+                .build();
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> productService.decreaseStock(requestDTO));
+        assertEquals("해당 상품번호와 일치하는 정보가 없습니다: " + prdNo, exception.getMessage());
+
     }
 
     @DisplayName("상품재고차감 실패: 재고부족")
